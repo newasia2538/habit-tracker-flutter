c809964c92e6218651abdced64bada1f3fbb2a4d
athiporn <11356979@krungsri.com>
2025-02-13 20:50:13 +0700

    completed decoration TimeLineView component

--- habit_tracking_flutter/lib/ui/components/timeline_view.dart
+++ habit_tracking_flutter/lib/ui/components/timeline_view.dart
@@ -14,13 +14,49 @@ class TimelineView extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 16),
       child: EasyDateTimeLine(
-        initialDate: selectedDate,
-        onDateChange: onSelectedDate,
-        headerProps: EasyHeaderProps(
-            monthPickerType: MonthPickerType.dropDown,
-            showHeader: false,
-            showSelectedDate: true),
-      ),
+          initialDate: selectedDate,
+          onDateChange: onSelectedDate,
+          headerProps: EasyHeaderProps(
+              monthPickerType: MonthPickerType.dropDown,
+              showHeader: false,
+              showSelectedDate: true),
+          dayProps: EasyDayProps(
+              dayStructure: DayStructure.dayNumDayStr,
+              activeDayStyle: DayStyle(
+                  decoration: BoxDecoration(
+                      color: colorScheme.primary,
+                      borderRadius: BorderRadius.circular(12),
+                      gradient: LinearGradient(
+                          colors: [colorScheme.primary, colorScheme.secondary],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter)),
+                  dayStrStyle: TextStyle(
+                      color: colorScheme.onPrimary,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
+                  dayNumStyle: TextStyle(
+                      color: colorScheme.onPrimary,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold)),
+              inactiveDayStyle: DayStyle(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: colorScheme.surface,
+                      border: Border.all(
+                          color: colorScheme.outlineVariant, width: 1)),
+                  dayStrStyle:
+                      TextStyle(color: colorScheme.onSurface, fontSize: 16),
+                  dayNumStyle:
+                      TextStyle(color: colorScheme.onSurface, fontSize: 16)),
+              todayHighlightStyle: TodayHighlightStyle.withBorder,
+              todayHighlightColor:
+                  colorScheme.primaryContainer.withOpacity(0.8),
+              todayStyle: DayStyle(
+                  dayStrStyle:
+                      TextStyle(color: colorScheme.primary, fontSize: 16),
+                  dayNumStyle:
+                      TextStyle(color: colorScheme.primary, fontSize: 16))),
+          timeLineProps: EasyTimeLineProps(separatorPadding: 16)),
     );
   }
 }
