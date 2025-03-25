e89b2af4595eebe01af814e52752cac15f5fa51e
athiporn <11356979@krungsri.com>
2025-03-26 00:59:10 +0700

    setup local database and add empty provider folder

--- habit_tracking_flutter/lib/data/database/database.dart
+++ habit_tracking_flutter/lib/data/database/database.dart
@@ -5,6 +5,7 @@ import 'package:drift/native.dart';
 import 'package:habit_tracking_flutter/data/database/tables.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
+import 'package:rxdart/rxdart.dart';
 
 part 'database.g.dart';
 
@@ -17,6 +18,30 @@ class AppDatabase extends _$AppDatabase {
   Stream<List<Habit>> watchHabits() => select(habits).watch();
 
   Future<int> createHabit(HabitsCompanion habit) => into(habits).insert(habit);
+
+  Stream<List<HabitWithCompletions>> watchHabitForDate(DateTime date) {
+    final stateDate = DateTime(date.year, date.month, date.day);
+    final endDate = DateTime(date.year, date.month, date.day, 23, 59, 59, 999);
+
+    final query = select(habits).join([
+      leftOuterJoin(
+          habitCompletions,
+          habitCompletions.habitId.equalsExp(habits.id) &
+              habitCompletions.completionDate
+                  .isBetweenValues(stateDate, endDate))
+    ]);
+
+    return query.watch().map((rows) {
+      return rows.map((row) {
+        final habit = row.readTable(habits);
+        final completion = row.readTableOrNull(habitCompletions);
+
+        return HabitWithCompletions(
+            habit: habit, isCompleted: completion != null);
+      }).toList();
+    });
+  }
+
   Future<bool> updateHabit(HabitsCompanion habit) =>
       update(habits).replace(habit);
   DeleteStatement<$HabitsTable, Habit> deleteHabit(int id) =>
@@ -50,6 +75,28 @@ class AppDatabase extends _$AppDatabase {
           .toCompanion(true));
     });
   }
+
+  Stream<(int, int)> watchDailySummary(DateTime date) {
+    final stateDate = DateTime(date.year, date.month, date.day);
+    final endDate = DateTime(date.year, date.month, date.day, 23, 59, 59, 999);
+
+    final completionStream = (select(habitCompletions)
+          ..where((t) => t.completionDate
+              .isBetween(Variable(stateDate), Variable(endDate))))
+        .watch();
+
+    final habitStream = watchHabitForDate(date);
+
+    return Rx.combineLatest2(completionStream, habitStream,
+        (completion, habit) => (completion.length, habit.length));
+  }
+}
+
+class HabitWithCompletions {
+  final Habit habit;
+  final bool isCompleted;
+
+  HabitWithCompletions({required this.habit, required this.isCompleted});
 }
 
 LazyDatabase _openConnection() {
